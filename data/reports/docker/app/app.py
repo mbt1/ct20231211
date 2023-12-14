a52b20f2cee4e684e80524cefe0a316a225fd12e
“mbt1” <mbt1@users.noreply.github.com>
2023-12-14 20:53:20 +0000

    report is now fully generated

--- data/reports/docker/app/app.py
+++ data/reports/docker/app/app.py
@@ -1,5 +1,8 @@
 import boto3
 from datetime import datetime
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert import NotebookExporter
 
 bucket_name = 'ct20231211-reports'
 
@@ -15,9 +18,33 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
         return False
     return True
 
+def process_ipynb(ipynb_path, replacements):
+    with open(ipynb_path, 'r') as file:
+        nb = nbformat.read(file, as_version=4)
 
-x = datetime.now()
-file_name = x.strftime('%Y-%m-%d-%H-%M-%S.txt')
+    for cell in nb['cells']:
+        if cell['cell_type'] == 'markdown':
+            for search_str,replace_str in replacements.items():
+                cell['source'] = cell['source'].replace(search_str,replace_str)
+
+    # Clear existing outputs and execute all cells
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb)
+
+    return nb
+
+def write_ipynb(nb,file):
+    nbformat.write(nb, file)
+
+current_date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+file_name = "report-"+current_date+".ipynb"
+
+print("processing ",file_name,"...")
+replacements = {
+    '<!--CT20231211FileDate-->': current_date
+    }
 with open(file_name, 'w') as fp:
-    print('hello world', file_name)
+    write_ipynb(process_ipynb('ReportTemplate.ipynb', replacements),fp)
+print("uploading ",file_name,"...")
 upload_file_to_s3(file_name, bucket_name)
+print("finished.")
