31c7458e9919f6b28ffa35fceea92936b7d02b7d
“mbt1” <mbt1@users.noreply.github.com>
2023-12-17 04:57:22 +0000

    dropdown now causes notebook to load - but not yet to display...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React,{useState, useEffect} from 'react';
+import ViewNotebook from 'react-jupyter-notebook';
+
+const bucket_url = 'https://ct20231211-reports.s3.amazonaws.com/'
 
 function App() {
   const [files, setFiles] = useState([]);
+  const [selectedFileURL, setSelectedFile] = useState('');
+  const [notebookContent, setNotebookContent] = useState(null);
 
   useEffect(() => {
     const fetchFiles = async () => {
       try {
-        const response = await fetch('https://ct20231211-reports.s3.amazonaws.com/');
+        const response = await fetch(bucket_url);
         const data = await response.text();
         parseXML(data);
       } catch (error) {
@@ -22,10 +27,37 @@ function App() {
     const parser = new DOMParser();
     const xml = parser.parseFromString(xmlData, "application/xml");
     const elements = xml.getElementsByTagName("Key");
-    const fileNames = Array.from(elements).map(element => element.textContent);
+    const fileNames = Array.from(elements).map(element => element.textContent).sort((a,b)=>b.localeCompare(a));
     setFiles(fileNames);
+    if (fileNames.length > 0) {
+      setSelectedFile(bucket_url+fileNames[0]);
+    }
   };
-  
+
+  const handleFileChange = (event) => {
+    console.log(1, event.target.value)
+    setSelectedFile(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchNotebook = async (NotebookURL) => {
+      try {
+        console.log(3, NotebookURL)
+        const response = await fetch(NotebookURL);
+        const data = await response.json();
+        console.log(4,data)
+        setNotebookContent(data);
+      } catch (error) {
+        console.error("Error fetching notebook: ", error);
+        setNotebookContent(null);
+      }
+    };
+    console.log(2,selectedFileURL)
+    if(null != selectedFileURL){
+      fetchNotebook(selectedFileURL);
+    }
+  }, [selectedFileURL]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -35,14 +67,24 @@ function App() {
         </h1>
       </header>
       <div>
-        <select>
+        <select value={selectedFileURL} onChange={handleFileChange}>
           {files.map(file => (
-            <option key={file} value={file}>{file}</option>
+            <option key={file} value={bucket_url+file}>{file}</option>
           ))}
         </select>
+        {!(null==notebookContent) &&<ViewNotebook notebook={notebookContent} />}
       </div>
     </div>
   );
 }
 
+// function DisplayNotebook(props) {
+//   console.log(5, props.json)
+//   if(null==props.json){
+//     console.log(5,"is NULL")
+//     return <></>
+//   }
+//   console.log(5,"is not NULL")
+//   return <><ViewNotebook notebook={props.json} /></>
+// }
 export default App;
